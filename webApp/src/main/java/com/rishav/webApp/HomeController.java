77da88d011d2afc80787a807baa75e147a6ace7b
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-01-27 19:04:39 +0530

    Spring Boot Web

---
+++ webApp/src/main/java/com/rishav/webApp/HomeController.java
@@ -0,0 +1,21 @@
+package com.rishav.webApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+//    @RequestMapping("/")
+//    public String greet(){
+//        return "Hello World!";
+//    }
+    //in good old days spring used to return a view name, but now it returns the string itself
+//    we can achieve the same by annoting @responsebody at the method or @RestController
+//    at the controller class
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World!";
+    }
+}
---
+++ webApp/src/test/java/com/rishav/webApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishav.webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
