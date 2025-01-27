fbe957b86e9cfbeddf23c5e9f9171b2858056d0e
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-01-27 17:40:04 +0530

    Setter Injection

--- DemoSpring/src/main/java/org/rishav/App.java
+++ DemoSpring/src/main/java/org/rishav/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-//        Dev obj=(Dev)context.getBean("dev");
+        Dev obj=(Dev)context.getBean("dev");
         //instaed of usnig Dev.class we can directly pass the class id which is defined on
         //the spring xml, but we need to explicitly type cast the object into the required class.
 //        obj.build();
-//        System.out.println( "Hello World!" );
+        System.out.println( obj.getAge() );
     }
 }
--- DemoSpring/src/main/java/org/rishav/Dev.java
+++ DemoSpring/src/main/java/org/rishav/Dev.java
@@ -1,8 +1,16 @@
 package org.rishav;
 
 public class Dev {
+    private int age;
     public Dev(){
         System.out.println("Dev object created!");
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
     }
     public void build(){
         System.out.println("Building the project...");
