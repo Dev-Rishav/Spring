a54f6efda0c855059cf9feedae76ed55226db7b2
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-01-27 20:45:32 +0530

    Get and Post Mapping

--- webApp/src/main/java/com/rishav/webApp/controller/ProductController.java
+++ webApp/src/main/java/com/rishav/webApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.rishav.webApp.controller;
 import com.rishav.webApp.model.Product;
 import com.rishav.webApp.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,4 +17,21 @@ public class ProductController {
         return service.getProducts();
     }
 
+    /****** 1. GET Method *****/
+
+    //{prodId} will act as :id of Node
+    //PathVariable annotation is used to bind the value of the template variable to the method parameter
+    @GetMapping("/products/{prodId}")
+    //By default the request mapping is of get type
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+    }
+
+    /****** 2. POST Method *****/
+    @PostMapping("/products")
+    //now this will overlap with the get method so we can use GetMapping, PostMapping,etc
+    public void addProduct(@RequestBody  Product prod){
+        //@requestBody annotation is used to bind the request body with a method parameter
+        service.addProduct(prod);
+    }
 }
--- webApp/src/main/java/com/rishav/webApp/services/ProductService.java
+++ webApp/src/main/java/com/rishav/webApp/services/ProductService.java
@@ -3,17 +3,33 @@ package com.rishav.webApp.services;
 import com.rishav.webApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products= Arrays.asList(new Product(101,"Iphone",50000),
+    List<Product> products=new ArrayList<>(Arrays.asList(new Product(101,"Iphone",50000),
             new Product(102,"Samsung",40000),
-            new Product(103,"OnePlus",30000));
+            new Product(103,"OnePlus",30000)));
 
     public List<Product> getProducts(){
         return products;
     }
+
+    public Product getProductById(int prodId){
+        return products.stream().filter(t->t.getProdId()==prodId).findFirst().get();
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
+    }
+
+    //@Override
+//    public String toString() {
+//        return "ProductService{" +
+//                "products=" + products +
+//                '}';
+//    }
 }
