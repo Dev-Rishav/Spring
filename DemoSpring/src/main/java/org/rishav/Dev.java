6c82d4b948f391841942e16189e0b9945101dd99
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-01-27 17:29:37 +0530

    Spring XML Config

--- DemoSpring/src/main/java/org/rishav/Dev.java
+++ DemoSpring/src/main/java/org/rishav/Dev.java
@@ -1,6 +1,9 @@
 package org.rishav;
 
 public class Dev {
+    public Dev(){
+        System.out.println("Dev object created!");
+    }
     public void build(){
         System.out.println("Building the project...");
     }
---
+++ DemoSpring/src/main/java/org/rishav/Laptop.java
@@ -0,0 +1,10 @@
+package org.rishav;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop obj created.");
+    }
+    public void compile(){
+        System.out.println("Compiling the project!!");
+    }
+}
