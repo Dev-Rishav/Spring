3d43889b28fcc3632cacc95cd38b0d5c09e849db
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-02-04 11:28:25 +0530

    Custom username and password

--- SpringSecurity/src/main/java/com/rishav/SpringSecurity/HelloController.java
+++ SpringSecurity/src/main/java/com/rishav/SpringSecurity/HelloController.java
@@ -1,12 +1,13 @@
 package com.rishav.SpringSecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String greet(){
-        return  "  Hello duniya";
+    public String greet(HttpServletRequest request){
+        return  "  Hello duniya \n"+ request.getSession().getId();
     }
 }
